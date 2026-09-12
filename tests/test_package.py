20ca76168f9606596dc13be5d54bb278d22927ac
Mohammad Hossein Zakeri <mohammadhosseinzakeri10@gmail.com>
2026-09-12 15:51:43 +0330

    project-foundation
    chore: establish project foundation

---
+++ tests/test_package.py
@@ -0,0 +1,8 @@
+"""Tests for the base Asena package."""
+
+import asena
+
+
+def test_package_imports() -> None:
+    """Ensure the Asena package can be imported successfully."""
+    assert asena is not None
